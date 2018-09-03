294152904fff8c27ae2937e6cdc6fe6742d98a59
katy@karstaway.com <katy@karstaway.com>
2018-09-03 12:45:14 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,8 +20,6 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1";
-#   SELECT COUNT([column name]) FROM [table name] WHERE [column name] = [value]
-# "SELECT color COUNT(color) FROM bears GR color order by count(color) ;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
