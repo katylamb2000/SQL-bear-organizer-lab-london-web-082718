5feee9c86331158cc873605883b9f7df32d76494
katy@karstaway.com <katy@karstaway.com>
2018-09-03 12:36:42 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,8 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM bears ORDER BY colour DESC LIMIT 1";
+  "SELECT COUNT(color) FROM bears WHERE colour = 'dark brown'";
+
 end
 
 def counts_number_of_bears_with_goofy_temperaments
